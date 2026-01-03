a46560ab2e77ba06151784b4c6007946aa25740d
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-04 03:08:37 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int group = 0;
+
+
+        for (int i = 0; i < N; i++) {
+            boolean b = false; // 멀리 중복이면 true
+
+            char[] inputArray = br.readLine().toCharArray();
+
+            for (int j = 1; j <= inputArray.length; j++) {
+                if (j < inputArray.length && inputArray[j - 1] != inputArray[j]) {
+
+                    // 하나 기준으로 떨어져 있는 거 중복 확인
+                    for (int k = j+1; k < inputArray.length; k++) {
+                        if (inputArray[j - 1] == inputArray[k]) {
+                            b = true;
+                            break;
+                        }
+                    }
+
+                } else if (j == 1 && j == inputArray.length)  {
+                    group++;
+                    b = true;
+                }
+            }
+            if (!b) group++;
+        }
+
+        System.out.print(group);
+    }
+}
