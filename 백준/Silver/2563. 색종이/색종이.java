4c25718842fbb3ac59040aebaad8390f75fa5d04
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-04 21:18:04 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[100][100]; // 초깃값 false
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    arr[x + j][y + k] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) {
+                    sum++;
+                }
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
