bf85f003af26812c1258f90abccaac47a167213b
vv01011108 <vv01011108@gmail.com>
2026-01-04 21:20:09 +0900

    260104

--- src/boj/TwoDimArray/p2563/Main.java
+++ src/boj/TwoDimArray/p2563/Main.java
@@ -1,4 +1,4 @@
-package boj.TwoDimArray.p2563;
+package boj.TwoDimArray.p2563; // 1h
 
 import java.util.*;
 import java.io.*;
@@ -7,5 +7,30 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[100][100]; // 초깃값 false
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    arr[x + j][y + k] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) {
+                    sum++;
+                }
+            }
+        }
+
+        System.out.print(sum);
     }
 }
