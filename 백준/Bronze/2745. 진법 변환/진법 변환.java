e14cd4f509756563d18ca908b91d6724622522f9
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-05 17:06:51 +0900

    [Bronze II] Title: 진법 변환, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String s = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char N = s.charAt(i);
+            if ('0' <= N && N <= '9') {
+                sum += (int)Math.pow(B, s.length() - i - 1) * (N - '0');
+            } else if ('A' <= N && N <= 'Z') {
+                sum += (int)Math.pow(B, s.length() - i - 1) * (N - 'A' + 10);
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
