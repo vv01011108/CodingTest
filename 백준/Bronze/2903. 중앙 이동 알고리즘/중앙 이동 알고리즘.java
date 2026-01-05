3314f122fa7c8af920d1f6e331f13f866f9cc0e6
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-06 02:29:32 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int row= 3;
+
+        if (N != 1) {
+            for (int i = 1; i < N; i++) {
+                row += row - 1;
+            }
+        }
+
+        System.out.print(row * row);
+    }
+}
