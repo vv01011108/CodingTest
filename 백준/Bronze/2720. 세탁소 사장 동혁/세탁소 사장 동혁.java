74009223de710d3eda16c97ebdcea3105d2bef44
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-06 01:23:54 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 96 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int quarter = 0;
+        int dime = 0;
+        int nickel = 0;
+        int penny = 0;
+        
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            quarter = input / 25;
+            dime = (input % 25) / 10;
+            nickel = (input % 25 % 10) / 5;
+            penny = input % 25 % 10 % 5;
+
+            sb.append(quarter).append(" ")
+                    .append(dime).append(" ")
+                    .append(nickel).append(" ")
+                    .append(penny).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
