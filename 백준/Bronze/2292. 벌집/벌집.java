9ee44beac8240b65f89cc48a736fa8f6b7ced918
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-06 03:20:04 +0900

    [Bronze II] Title: 벌집, Time: 96 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int sum = 1;
+        int N = 1;
+
+        while (input > sum) {
+            sum += 6 * N;
+            N++;
+        }
+        System.out.print(N);
+    }
+}
