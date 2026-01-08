53e821f3ea62425c58e7c4fc0138e8abf4df45dc
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-08 18:22:13 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -1,11 +1,7 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
         System.out.println(1);
         System.out.print(0);
     }
