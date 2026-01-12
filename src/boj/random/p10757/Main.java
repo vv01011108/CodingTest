1de05b2400be55ecbbed3ec4d55109fca9eb2c22
vv01011108 <vv01011108@gmail.com>
2026-01-12 23:34:58 +0900

    260112

---
+++ src/boj/random/p10757/Main.java
@@ -0,0 +1,15 @@
+package boj.random.p10757;
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        System.out.print(A.add(B));
+    }
+}
