80a97a06c926807b8fdc6f63d422bde00acdf387
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-14 23:08:06 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        String[] board = new String[N];
+        for (int i = 0; i < N; i++)
+            board[i] = br.readLine();
+
+        int answer = Integer.MAX_VALUE;
+        
+        for (int x = 0; x <= N - 8; x++) {
+            for (int y = 0; y <= M - 8; y++) {
+
+                int countW = 0;
+                int countB = 0;
+
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        char current = board[x + i].charAt(y + j);
+
+                        char expectedW = ((i + j) % 2 == 0) ? 'W' : 'B';
+                        char expectedB = ((i + j) % 2 == 0) ? 'B' : 'W';
+
+                        if (current != expectedW) countW++;
+                        if (current != expectedB) countB++;
+                    }
+                }
+
+                int localMin = Math.min(countW, countB);
+                answer = Math.min(answer, localMin);
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
