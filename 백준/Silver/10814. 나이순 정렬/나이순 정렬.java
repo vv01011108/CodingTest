58a11003835bcc50fa3636d2c87227d9f521107b
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-21 01:28:40 +0900

    [Silver V] Title: 나이순 정렬, Time: 728 ms, Memory: 58148 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = st.nextToken();
+            }
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (!a[0].equals(b[0])) {
+                return Integer.parseInt(a[0]) - Integer.parseInt(b[0]);
+            } else {
+                return 0;
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
