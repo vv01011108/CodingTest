02edbac38b992655cb1016fa913d2a177d998634
vv01011108 <vv01011108@gmail.com>
2026-01-20 23:20:09 +0900

    250120

---
+++ src/boj/sort/p11651/Main.java
@@ -0,0 +1,34 @@
+package boj.sort.p11651;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] != b[1]) {
+                return a[1] - b[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+}
