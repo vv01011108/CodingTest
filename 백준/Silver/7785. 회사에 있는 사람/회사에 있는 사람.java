6a7bb2ea1261e9640e6a06094e4a95c600cd8964
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-21 18:24:54 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1192 ms, Memory: 55764 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String state = st.nextToken();
+
+            if (state.equals("enter")) {
+                set.add(name);
+            } else {
+                set.remove(name);
+            }
+        }
+
+        List<String> arr = new ArrayList<>(set);
+        arr.sort(Collections.reverseOrder());
+
+        for (String x : arr) {
+            System.out.println(x);
+        }
+    }
+}
