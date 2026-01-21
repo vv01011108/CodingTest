9964a422e10e78a9652f8ed75471c0f4d81a6aa3
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-01-21 17:40:47 +0900

    [Silver IV] Title: 문자열 집합, Time: 360 ms, Memory: 31416 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> S = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            S.add(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            if (S.contains(br.readLine())) count++;
+        }
+
+        System.out.print(count);
+    }
+}
