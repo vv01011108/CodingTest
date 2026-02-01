6e67fb8d0e35de0e2551a94490c775c9f6e34482
vv01011108 <vv01011108@gmail.com>
2026-02-02 00:02:39 +0900

    260130

---
+++ src/boj/prime2/p13241/Main.java
@@ -0,0 +1,4 @@
+package boj.prime2.p13241;
+
+public class Main {
+}
---
+++ src/boj/prime2/p1934/Main.java
@@ -0,0 +1,32 @@
+package boj.prime2.p1934;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int AA = A;
+            int B = Integer.parseInt(st.nextToken());
+            int BB = B;
+
+            while (B != 0) {
+               int r = A % B;
+               A = B;
+               B = r;
+            }
+
+            sb.append((AA/A) * (BB/A) * A).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
