a8bf5ead28481ef6f522afa284ecc46865484074
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-02-02 00:35:17 +0900

    [Silver III] Title: 분수 합, Time: 124 ms, Memory: 16080 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        // 분모
+        int m = B * D;
+
+        // 분자
+        int n = A * D + B * C;
+
+        // 약분
+        while (n != 0) {
+            int tmp = n;
+            n = m % n;
+            m = tmp;
+        }
+
+        System.out.print((A * D + B * C) / m + " " + (B * D) / m);
+    }
+}
