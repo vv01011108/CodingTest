de6116cf7395a2ada6ff80c658b3a62bad1a928b
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-02-02 00:19:19 +0900

    [Silver V] Title: 최소공배수, Time: 92 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        long AA = A;
+        long BB = B;
+
+        while(B != 0) {
+            long tmp = B;
+            B = A % B;
+            A = tmp;
+        }
+
+        System.out.print(A * (AA/A) * (BB/A));
+
+
+    }
+}
