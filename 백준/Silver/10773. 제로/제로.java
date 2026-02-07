dc18a2cde84a9fd308112c585000bed0e18223ad
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-02-07 22:13:10 +0900

    [Silver IV] Title: 제로, Time: 188 ms, Memory: 24368 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        long sum = 0;
+        int pre = 0;
+
+        for (int i = 0; i < N; i++) {
+            int K = Integer.parseInt(br.readLine());
+            if (K != 0) {
+                stack.push(K);
+                sum += K;
+            } else {
+                sum -= stack.pop();
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
