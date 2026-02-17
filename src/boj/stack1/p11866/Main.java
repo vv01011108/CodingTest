6f175c3efc86afb0d560a583ddb7706154cacce9
vv01011108 <vv01011108@gmail.com>
2026-02-18 00:16:16 +0900

    260218

--- src/boj/stack1/p11866/Main.java
+++ src/boj/stack1/p11866/Main.java
@@ -1,4 +1,33 @@
 package boj.stack1.p11866;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            for (int j = 1; j < K; j++) {
+                queue.addLast(queue.pollFirst());
+            }
+
+            sb.append(queue.poll());
+
+            if (!queue.isEmpty())
+                sb.append(", ");
+        }
+
+        System.out.print("<" + sb + ">");
+    }
 }
