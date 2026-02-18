1ca077fe31f8d076f860060ccd84354fc0297cda
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-02-19 01:22:25 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 148 ms, Memory: 15132 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<int[]> deque = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            deque.addLast(new int[]{i, x});
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int[] curr = deque.peekFirst();
+            sb.append(curr[0]).append(" ");
+            deque.pollFirst();
+
+            if (!deque.isEmpty()) {
+                if (curr[1] > 0) {
+                    for (int j = 0; j < curr[1] - 1; j++) {
+                        deque.addLast(deque.pollFirst());
+                    }
+                } else if (curr[1] < 0) {
+                    for (int j = curr[1]; j < 0; j++) {
+                        deque.addFirst(deque.pollLast());
+                    }
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
