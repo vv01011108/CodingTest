77914ada4d028d9d118893a375e5a57df219c805
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-02-24 18:18:37 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int N = Integer.parseInt(br.readLine());
+
+        System.out.print(N * (N-1));
+    }
+}
