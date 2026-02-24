b27f5f1732ce669cd2c326944d25ff04f39f2b26
vv01011108 <vv01011108@gmail.com>
2026-02-24 18:06:06 +0900

    260222

---
+++ src/boj/stack1/p24511/Main.java
@@ -0,0 +1,40 @@
+package boj.stack1.p24511;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // A = 0 큐, A = 1 스택
+        int[] A = new int[N];
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (A[i] == 0) {
+                deque.addLast(x);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            deque.addFirst(input);
+            sb.append(deque.pollLast()).append(" ");
+        }
+
+        System.out.print(sb);
+    }
+}
