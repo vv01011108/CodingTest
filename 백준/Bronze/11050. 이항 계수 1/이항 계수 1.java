e1fd4f98fe224a7396dda708d899cc9b56c3fee8
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-02-24 18:47:12 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int n = 1;
+        for (int i = 2; i <= N; i++) {
+            n *= i;
+        }
+
+        int k = 1;
+        for (int i = 2; i <= K; i++) {
+            k *= i;
+        }
+
+        int minus = 1;
+        for (int i = 2; i <= N - K; i++) {
+            minus *= i;
+        }
+
+        System.out.print(n / (k * minus));
+    }
+}
