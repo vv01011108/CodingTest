6179ddd5c94806fd1059a34672bc52831d93f8e4
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-03-11 16:39:09 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 112 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(N));
+    }
+}
