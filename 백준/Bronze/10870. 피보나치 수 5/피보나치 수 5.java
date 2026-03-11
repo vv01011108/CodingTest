7a699ba4ac5b5f70aace1b171fa306dbef67cb1e
Park seong a <126747091+vv01011108@users.noreply.github.com>
2026-03-11 16:50:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static int fibon(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibon(n - 1) + fibon(n - 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print(fibon(N));
+    }
+}
