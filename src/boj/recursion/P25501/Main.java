c356e4456b96b6bf49c1608dd8e90a25e304381c
vv01011108 <vv01011108@gmail.com>
2026-03-15 17:52:27 +0900

    260315

--- src/boj/recursion/P25501/Main.java
+++ src/boj/recursion/P25501/Main.java
@@ -4,7 +4,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    static int count = 0;
+
+    public static int recursion(String s, int l, int r) {
+        count++;
+        if (l >= r) {
+            return 1;
+        } else if (s.charAt(l) != s.charAt(r)) {
+            return 0;
+        }
+        else return recursion(s, l + 1, r - 1);
+    }
+
+    public static int isPalindrome(String s) {
+        count = 0;
+        return recursion(s, 0, s.length() - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String word = br.readLine();
+            sb.append(isPalindrome(word)).append(" ").append(count).append("\n");
+        }
+
+        System.out.print(sb);
     }
 }
