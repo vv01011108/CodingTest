e1aa97a7462cd8625faffa32934ffc77b8a47aa4
vv01011108 <vv01011108@gmail.com>
2026-03-16 23:27:18 +0900

    260316

---
+++ src/boj/random/p1032/Main.java
@@ -0,0 +1,36 @@
+package boj.random.p1032;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr[0].length(); i++) {
+            boolean same = true;
+
+            for (int j = 0; j < N - 1; j++) {
+                if (arr[j].charAt(i) != arr[j + 1].charAt(i)) {
+                    same = false;
+                    break;
+                }
+            }
+
+            if (same) {
+                sb.append(arr[0].charAt(i));
+            } else {
+                sb.append("?");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
---
+++ src/boj/recursion/p24060/Main.java
@@ -0,0 +1,10 @@
+package boj.recursion.p24060;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
